df611647f146aef3b8e4c49f2fb67352fa2c403e
yxj0312 <yanxiajun@hotmail.com>
2020-07-26 23:35:55 +0200

    Redux-thunk

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
+import thunk from "redux-thunk";
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(forbiddenWordsMiddleware)
+    storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
 );
 
 export default store;
